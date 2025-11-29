67695f8e04419e69f80d0c88617f00d186937342
Jason Pell <jason@pellcorp.com>
2025-11-29 13:57:43 +1100

    remove wpa supplicant from config to env variable

--- src/utils.cpp
+++ src/utils.cpp
@@ -112,7 +112,7 @@ namespace KUtils {
   }
 
   std::string get_wifi_interface() {
-    std::string wpa_socket = Config::get_instance()->get<std::string>("/system/wpa_supplicant");
+    std::string wpa_socket = std::string(std::getenv("WPA_SUPPLICANT_SOCKET"));
     if (fs::is_directory(fs::status(wpa_socket))) {
       for (const auto &e : fs::directory_iterator(wpa_socket)) {
         if (fs::is_socket(e.path()) && e.path().string().find("p2p") == std::string::npos) {
@@ -120,7 +120,6 @@ namespace KUtils {
         }
       }
     }
-
     return "";
   }
 
