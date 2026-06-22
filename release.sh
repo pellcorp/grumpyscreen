12ce51f147953f17f82592c2c5698f6abd2669b7
Jason Pell <jason@pellcorp.com>
2026-06-22 11:10:47 +1000

    restore releases but only for main not pull requests

--- release.sh
+++ release.sh
@@ -1,42 +1,32 @@
 #!/bin/bash
 
-function do_release() {
-  RELEASES_DIR=./releases/build/grumpyscreen
-  rm -rf $RELEASES_DIR
-  mkdir -p $RELEASES_DIR
+RELEASES_DIR=./releases/grumpyscreen
+rm -rf $RELEASES_DIR
+mkdir -p $RELEASES_DIR
 
-  ASSET_NAME=$1
+ASSET_NAME=$1
+GIT_SHA=$2
+GIT_BRANCH=$3
 
-  cp ./build/bin/guppyscreen $RELEASES_DIR/grumpyscreen
-  cp grumpyscreen.cfg $RELEASES_DIR/
+TIMESTAMP=$(date +%s)
 
-  if [ "$ASSET_NAME" = "grumpyscreen-smallscreen" ]; then
-    sed -i 's/display_rotate: 3/display_rotate: 2/g' $RELEASES_DIR/grumpyscreen.cfg
-  elif [ "$ASSET_NAME" = "grumpyscreen-rpi" ]; then
-    sed -i 's/display_rotate: 3/display_rotate: 0/g' $RELEASES_DIR/grumpyscreen.cfg
-    # rpi does not have factory reset
-    sed -i 's:/etc/init.d/S58factoryreset reset::g' $RELEASES_DIR/grumpyscreen.cfg
-    sed -i 's:/etc/init.d/S99guppyscreen restart:sudo systemctl restart grumpyscreen:g' $RELEASES_DIR/grumpyscreen.cfg
-    sed -i 's:/etc/init.d/S55klipper_service restart:sudo systemctl restart klipper:g' $RELEASES_DIR/grumpyscreen.cfg
-    # rpi does not have switch to stock
-    sed -i 's:/usr/data/pellcorp/k1/switch-to-stock.sh::g' $RELEASES_DIR/grumpyscreen.cfg
-  fi
-  tar cf - -C releases/build . | pigz -9 > releases/${ASSET_NAME}.tar.gz
-}
+cp ./build/bin/guppyscreen $RELEASES_DIR/grumpyscreen
+cp grumpyscreen.cfg $RELEASES_DIR/
+echo "GIT_SHA=${GIT_SHA:0:7}" > $RELEASES_DIR/release.info
+echo "GIT_BRANCH=$GIT_BRANCH" >> $RELEASES_DIR/release.info
+echo "TIMESTAMP=$TIMESTAMP" >> $RELEASES_DIR/release.info
 
-[ -d ./releases/ ] && rm -rf ./releases
+echo "Timestamp was $TIMESTAMP"
 
-echo "Building grumpyscreen ..."
-./build.sh > /dev/null --setup mips || exit $?
-./build.sh > /dev/null || exit $?
-do_release grumpyscreen
-
-echo "Building grumpyscreen-smallscreen ..."
-./build.sh --setup mips --small > /dev/null  || exit $?
-./build.sh  > /dev/null  || exit $?
-do_release grumpyscreen-smallscreen
-
-echo "Building grumpyscreen-rpi ..."
-./build.sh --setup rpi > /dev/null  || exit $?
-./build.sh > /dev/null  || exit $?
-do_release grumpyscreen-rpi
+if [ "$ASSET_NAME" = "guppyscreen-smallscreen" ]; then
+  sed -i 's/display_rotate: 3/display_rotate: 2/g' $RELEASES_DIR/grumpyscreen.cfg
+elif [ "$ASSET_NAME" = "guppyscreen-rpi" ]; then
+  sed -i 's/display_rotate: 3/display_rotate: 0/g' $RELEASES_DIR/grumpyscreen.cfg
+  # rpi does not have factory reset
+  sed -i 's:/etc/init.d/S58factoryreset reset::g' $RELEASES_DIR/grumpyscreen.cfg
+  sed -i 's:/etc/init.d/S99guppyscreen restart:sudo systemctl restart grumpyscreen:g' $RELEASES_DIR/grumpyscreen.cfg
+  # rpi does not have switch to stock
+  sed -i 's:/usr/data/pellcorp/k1/switch-to-stock.sh::g' $RELEASES_DIR/grumpyscreen.cfg
+  sed -i 's:/etc/init.d/S55klipper_service restart:sudo systemctl restart klipper:g' $RELEASES_DIR/grumpyscreen.cfg
+fi
+tar czf $ASSET_NAME.tar.gz -C releases .
